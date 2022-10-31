841c0ac3aef7110a7850734fa2567c0b93c73f62
Steven <shelesteven@gmail.com>
2022-10-31 16:31:14 -0400

    Implemented decryption

--- decrypt.c
+++ decrypt.c
@@ -5,38 +5,56 @@
 
 #include "decrypt.h"
 
-char decryptChar(char comp1, char comp2) {
-    if (comp1 == 'T' && comp2 == 'T'){
+char decryptChar(char comp1, char comp2)
+{
+    if (comp1 == 'T' && comp2 == 'T')
+    {
         return '\t';
     }
 
     int res = fromHex(comp1) * 16 + fromHex(comp2);
     res += 16;
-    if (res > 127) {
+    if (res > 127)
+    {
         return (char)(res - 144 + 32);
     }
     return res;
 }
 
-int decryptFile(char* filename_in) {
+int decryptFile(char *filename_in)
+{
     printf("cryptoMagic: Decrypting %s\n", filename_in);
-    FILE* file_in = fopen(filename_in, "r");
-    if (!file_in) {
+    FILE *file_in = fopen(filename_in, "r");
+    if (!file_in)
+    {
         printf("cryptoMagic: Could not open input file!\n");
         return -1;
     }
     int basename_len = 0;
-    char* basename = getFileBaseName(filename_in, &basename_len);
-    char* filename_out = malloc(basename_len + 5); // 4 + 1
-    snprintf(filename_out, basename_len, "%s%s", basename, ".txt");
+    char *basename = getFileBaseName(filename_in, &basename_len);
+    char *filename_out = malloc(basename_len + 5); // 4 + 1
+    snprintf(filename_out, basename_len + 5, "%s%s", basename, ".txt");
     free(basename);
-    FILE* file_out = fopen(filename_out, "w");
+    FILE *file_out = fopen(filename_out, "w");
     free(filename_out);
-    if (!file_in) {
+    if (!file_in)
+    {
         printf("cryptoMagic: Could not open output file!\n");
         return -1;
     }
-    
+
+    char c1, c2;
+    while ((c1 = fgetc(file_in)) != EOF)
+    {
+        if (c1 != '\n')
+        {
+            c2 = fgetc(file_in);
+            fprintf(file_out, "%c", decryptChar(c1, c2));
+            continue;
+        }
+        fprintf(file_out, "\n");
+    }
+
     fclose(file_out);
     fclose(file_in);
     return 0; // Success
--- encrypt.c
+++ encrypt.c
@@ -4,41 +4,52 @@
 #include "strtools.h"
 #include "encrypt.h"
 
-int encryptChar(char ascii_in) {
+int encryptChar(char ascii_in)
+{
     ascii_in -= 16;
-    if (ascii_in < 32){
+    if (ascii_in < 32)
+    {
         return ascii_in - 32 + 144;
     }
     return ascii_in;
 }
 
-int encryptFile(char* filename_in) {
+int encryptFile(char *filename_in)
+{
     printf("cryptoMagic: Encrypting %s\n", filename_in);
-    FILE* file_in = fopen(filename_in, "r");
-    if (!file_in) {
+    FILE *file_in = fopen(filename_in, "r");
+    if (!file_in)
+    {
         printf("cryptoMagic: Could not open input file!\n");
         return -1;
     }
     int basename_len = 0;
-    char* basename = getFileBaseName(filename_in, &basename_len);
-    char* filename_out = malloc(basename_len + 5); // 4 + 1
+    char *basename = getFileBaseName(filename_in, &basename_len);
+    char *filename_out = malloc(basename_len + 5); // 4 + 1
     snprintf(filename_out, basename_len + 5, "%s%s", basename, ".crp");
     printf("cryptoMagic: Encrypting to %s\n", filename_out);
     free(basename);
-    FILE* file_out = fopen(filename_out, "w");
+    FILE *file_out = fopen(filename_out, "w");
     free(filename_out);
-    if (!file_in) {
+    if (!file_in)
+    {
         printf("cryptoMagic: Could not open output file!\n");
         return -1;
     }
 
     char c = 0;
-    while ((c = fgetc(file_in)) != EOF) {
-        if (c == '\t') {
+    while ((c = fgetc(file_in)) != EOF)
+    {
+        if (c == '\t')
+        {
             fprintf(file_out, "TT");
-        } else if (c == '\n') {
+        }
+        else if (c == '\n')
+        {
             fprintf(file_out, "\n");
-        } else {
+        }
+        else
+        {
             fprintf(file_out, "%X", encryptChar(c));
         }
     }
